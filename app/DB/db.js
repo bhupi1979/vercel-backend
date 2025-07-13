43e8a11ec84900ad775c971ec717cc8742ea7250
bhupi1979 <bhupendrapaliwal1983@gmail.com>
2025-07-13 11:59:31 +0530

    the atlas database addedd

--- app/DB/db.js
+++ app/DB/db.js
@@ -1,7 +1,10 @@
 let mngs=require('mongoose')
 require('dotenv').config()
 const dbconnect=()=>{
-mngs.connect(process.env.URL).then(()=>{
+mngs.connect(process.env.URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=>{
 console.log("database connected successfuly")
 })
 }
